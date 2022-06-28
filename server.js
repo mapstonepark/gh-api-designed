1b89633c2964974fd3b62f84a4c6510f8cb6f5ac
Jamal Park <102711674+mapstonepark@users.noreply.github.com>
2022-06-28 14:39:58 -0500

    remove port process

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.port ||  8000
+const PORT =  8000
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
